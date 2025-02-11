eb08c98b791fbf89cf134e68d9bcaca6f4a4d812
ssprea <188109862+ssprea@users.noreply.github.com>
2025-02-11 23:50:34 +0100

    added support for custom main window main grid xaml

--- ssprea-nvidia-control/Views/MainWindow.axaml.cs
+++ ssprea-nvidia-control/Views/MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
@@ -50,10 +51,77 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         });
 
         WindowsManager.AllWindows.Add(this);
+
+        LoadGuiGrid();
         
+
+    }
+
+    private async Task DownloadDefaultGuiAsync()
+    {
+        Console.WriteLine("Default GUI does not exist! Downloading from https://gist.githubusercontent.com/ssprea/d16e12733bca0db0107ccd51e4dfa4c3/raw/73295ca1ed0dda84e9c2877aa782d280b2dbc1ae/MainWindowMainGrid.axaml");
+        using (var c = new HttpClient())
+        {
+            try
+            {
+                var response = await c.GetAsync(
+                    "https://gist.githubusercontent.com/ssprea/d16e12733bca0db0107ccd51e4dfa4c3/raw/73295ca1ed0dda84e9c2877aa782d280b2dbc1ae/MainWindowMainGrid.axaml");
+                    
+                var download = await response.Content.ReadAsStringAsync();
+                
+                await File.WriteAllTextAsync($"{Program.DefaultDataPath}/Guis/Default/MainWindowMainGrid.axaml", download);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine($"Automatic download failed! Try downloading the file manually and put it in: {Program.DefaultDataPath}/Guis/Default/MainWindowMainGrid.axaml");
+                Environment.Exit(1);
+            }
+
+
+                
+        }
     }
     
-   
+    private void LoadGuiGrid()
+    {
+        
+        if (!File.Exists($"{Program.DefaultDataPath}/Guis/Default/MainWindowMainGrid.axaml"))
+        {
+            if (!Directory.Exists($"{Program.DefaultDataPath}/Guis/Default/"))
+            {
+                Directory.CreateDirectory($"{Program.DefaultDataPath}/Guis/Default/");
+            }
+            Task.Run(DownloadDefaultGuiAsync).Wait();
+        }
+        
+        //read selected gui name
+        if (!File.Exists(Program.DefaultDataPath+"/SelectedGui.txt") || File.ReadAllText(Program.DefaultDataPath+"/SelectedGui.txt") == string.Empty)
+        {
+            File.WriteAllText(Program.DefaultDataPath+"/SelectedGui.txt", "Default");
+        }
+        
+        var selectedGuiName = (File.ReadAllText(Program.DefaultDataPath+"/SelectedGui.txt")).Trim();
+        
+        
+        
+        //check if folder exists
+        if (!File.Exists(Program.DefaultDataPath + "/Guis/" + selectedGuiName+"/MainWindowMainGrid.axaml"))
+        {
+            Console.WriteLine("No GUI named \""+selectedGuiName+"\" found. Loading default.");
+            selectedGuiName = "Default";
+        }
+
+        var mainGridLoadPath = $"{Program.DefaultDataPath}/Guis/{selectedGuiName}/MainWindowMainGrid.axaml";
+        
+        var guiStream = File.OpenRead(mainGridLoadPath);
+        var loadedGrid = AvaloniaRuntimeXamlLoader.Load(guiStream) as Grid;
+        
+        
+        MainOcWindow.Content =  loadedGrid;
+        Console.WriteLine("Successfully loaded Gui from: "+mainGridLoadPath);
+        //Console.WriteLine("Successfully loaded Gui from: "+mainGridLoadPath);
+    }
     
     private async Task DoShowNewProfileDialogAsync(IInteractionContext<NewOcProfileWindowViewModel, OcProfile?> interaction)
     {
@@ -96,19 +164,7 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     //     comboBox.ItemsSource = viewModel.NvmlService.GpuList;
     //     comboBox.SelectedIndex = 0;
     // }
-
-
-    private void Window_OnClosing(object? sender, WindowClosingEventArgs e)
-    {
-        
-    }
-
-    private void FanComboBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
-    {
-        ViewModel.SelectedGpuFan = ((NvmlGpuFan)(e.AddedItems[0]));
-    }
     
-
 
     private void Window_OnClosed(object? sender, EventArgs e)
     {
