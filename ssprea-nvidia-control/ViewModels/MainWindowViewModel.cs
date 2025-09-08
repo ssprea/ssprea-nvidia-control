c89497f4cffec044c2eef18cbba2cddafee04638
ssprea <188109862+ssprea@users.noreply.github.com>
2025-09-08 16:21:30 +0200

    changed app main font to NotoSans for compatibility with different locales

--- ssprea-nvidia-control/ViewModels/MainWindowViewModel.cs
+++ ssprea-nvidia-control/ViewModels/MainWindowViewModel.cs
@@ -79,7 +79,9 @@ public partial class MainWindowViewModel : ViewModelBase
     
     //Axes styles for fan curve graph graph
     [ObservableProperty] private SolidColorPaint _graphTooltipTextPaint = new SolidColorPaint(SKColors.Black) {SKTypeface = _fanCurveGraphTypeface};
-    private static readonly SKTypeface _fanCurveGraphTypeface = SKTypeface.FromFamilyName("Noto Sans Mono",SKFontStyleWeight.Normal,SKFontStyleWidth.Normal,SKFontStyleSlant.Upright);
+    // private static readonly SKTypeface _fanCurveGraphTypeface = SKTypeface.FromFamilyName("Noto Sans Mono",SKFontStyleWeight.Normal,SKFontStyleWidth.Normal,SKFontStyleSlant.Upright);
+    private static readonly SKTypeface _fanCurveGraphTypeface =
+        SKTypeface.FromStream(AssetLoader.Open(new Uri("avares://ssprea-nvidia-control/Assets/Fonts/NotoSans/NotoSans-Light.ttf")));
     
     public Axis[] FanCurveGraphXAxes { get; set; } =
         [
--- ssprea-nvidia-control/ViewModels/UsageGraphsWindowViewModel.cs
+++ ssprea-nvidia-control/ViewModels/UsageGraphsWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Avalonia.Platform;
 using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
@@ -38,8 +39,8 @@ public partial class UsageGraphsWindowViewModel : ViewModelBase
     private readonly MaxSizeObservableCollection<int> _gpuUsageValues = new(_graphLength);
     private readonly MaxSizeObservableCollection<int> _memUsageValues = new(_graphLength);
     
-    private static SKTypeface _defaultGraphTypeface =  SKTypeface.FromFamilyName("Noto Sans Mono",SKFontStyleWeight.Normal,SKFontStyleWidth.Normal,SKFontStyleSlant.Upright);
-    
+    private static SKTypeface _defaultGraphTypeface =  SKTypeface.FromStream(AssetLoader.Open(new Uri("avares://ssprea-nvidia-control/Assets/Fonts/NotoSans/NotoSans-Light.ttf")));
+        
     [ObservableProperty] private SolidColorPaint _graphTooltipTextPaint = new SolidColorPaint(SKColors.Black) {SKTypeface = _defaultGraphTypeface};
     
     #region GraphStyles
@@ -202,22 +203,23 @@ public partial class UsageGraphsWindowViewModel : ViewModelBase
         GpuTempSeries[0] = new LineSeries<int>()
         {
             Values = _gpuTempValues,
-            Name = Lang.Resources.GraphsCoreTemp,
             Fill = new SolidColorPaint(SKColors.Green.WithAlpha(50)),
             Stroke = new SolidColorPaint(SKColors.Green) {StrokeThickness = 1},
             GeometryStroke = null,//new SolidColorPaint(SKColors.Green) {StrokeThickness = 4},
             GeometryFill = null,
+            YToolTipLabelFormatter = point => $"{Lang.Resources.GraphsCoreTemp}: {point.Model}°C",
+            LineSmoothness = 0,
             
-            LineSmoothness = 0
             // GeometrySize = 8
         };
         
         PowerUsageSeries[0] = new LineSeries<int>()
         {
             Values = _powerUsageValues,
-            Name = Lang.Resources.GraphsGpuPower,
             Fill = new SolidColorPaint(SKColors.MediumPurple.WithAlpha(50)),
             Stroke = new SolidColorPaint(SKColors.MediumPurple) {StrokeThickness = 1},
+            YToolTipLabelFormatter = point => $"{Lang.Resources.GraphsGpuPower}: {point.Model} W",
+            
             //GeometryStroke = new SolidColorPaint(SKColors.MediumPurple) {StrokeThickness = 4},
             GeometryStroke = null,
             GeometryFill = null,
@@ -230,13 +232,14 @@ public partial class UsageGraphsWindowViewModel : ViewModelBase
         GpuUsageSeries[0] = new LineSeries<int>()
         {
             Values = _gpuUsageValues,
-            Name = Lang.Resources.GraphsCoreUsage,
             GeometrySize = 8,
             GeometryStroke = null,
             GeometryFill = null,
             Fill = new SolidColorPaint(SKColors.Aqua.WithAlpha(50)),
             Stroke = new SolidColorPaint(SKColors.Aqua) {StrokeThickness = 1},
-            LineSmoothness = 0
+            LineSmoothness = 0,
+            YToolTipLabelFormatter = point => $"{Lang.Resources.GraphsCoreUsage}: {point.Model}%",
+            
             
             
         };
@@ -244,14 +247,15 @@ public partial class UsageGraphsWindowViewModel : ViewModelBase
         MemUsageSeries[0] = new LineSeries<int>()
         {
             Values = _memUsageValues,
-            Name = Lang.Resources.GraphsMemUsage,
             Fill = new SolidColorPaint(SKColors.Goldenrod.WithAlpha(50)),
             Stroke = new SolidColorPaint(SKColors.Goldenrod) {StrokeThickness = 1},
             // GeometryStroke = new SolidColorPaint(SKColors.Goldenrod) {StrokeThickness = 4},
             GeometrySize = 8,
             GeometryStroke = null,
             GeometryFill = null,
-            LineSmoothness = 0
+            LineSmoothness = 0,
+            YToolTipLabelFormatter = point => $"{Lang.Resources.GraphsMemUsage}: {point.Model}%",
+            
             
             
         };
@@ -259,14 +263,15 @@ public partial class UsageGraphsWindowViewModel : ViewModelBase
         GpuClockSeries[0] = new LineSeries<int>()
         {
             Values = _gpuClockValues,
-            Name = Lang.Resources.GraphsCoreClock,
             Fill = new SolidColorPaint(SKColors.DeepPink.WithAlpha(50)),
             Stroke = new SolidColorPaint(SKColors.DeepPink) {StrokeThickness = 1},
             // GeometryStroke = new SolidColorPaint(SKColors.DeepPink) {StrokeThickness = 4},
             GeometrySize = 8,
             GeometryStroke = null,
             GeometryFill = null,
-            LineSmoothness = 0
+            LineSmoothness = 0,
+            YToolTipLabelFormatter = point => $"{Lang.Resources.GraphsCoreClock}: {point.Model} MHz",
+            
             
             
         };
@@ -274,28 +279,30 @@ public partial class UsageGraphsWindowViewModel : ViewModelBase
         MemClockSeries[0] = new LineSeries<int>()
         {
             Values = _memClockValues,
-            Name = Lang.Resources.GraphsMemClock,
             Fill = new SolidColorPaint(SKColors.Chocolate.WithAlpha(50)),
             Stroke = new SolidColorPaint(SKColors.Chocolate) {StrokeThickness = 1},
             GeometryStroke = null,
             GeometryFill = null,
             //GeometryStroke = new SolidColorPaint(SKColors.Chocolate) {StrokeThickness = 4},
             GeometrySize = 8,
-            LineSmoothness = 0
+            LineSmoothness = 0,
+            YToolTipLabelFormatter = point => $"{Lang.Resources.GraphsMemClock}: {point.Model} MHz",
+            
             
         };
         
         FanSpeedSeries[0] = new LineSeries<int>()
         {
             Values = _fanSpeedValues,
-            Name = Lang.Resources.GraphsFanSpeed,
             Fill = new SolidColorPaint(SKColors.IndianRed.WithAlpha(50)),
             Stroke = new SolidColorPaint(SKColors.IndianRed) {StrokeThickness = 1},
             GeometryStroke = null,
             GeometryFill = null,
             //GeometryStroke = new SolidColorPaint(SKColors.IndianRed) {StrokeThickness = 4},
             GeometrySize = 8,
-            LineSmoothness = 0
+            LineSmoothness = 0,
+            YToolTipLabelFormatter = point => $"{Lang.Resources.GraphsFanSpeed}: {point.Model}%",
+            
             
             
         };
