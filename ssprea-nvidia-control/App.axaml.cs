db2023a7999e8e1500f7017d650584f2db48558e
ssprea <188109862+ssprea@users.noreply.github.com>
2026-08-02 16:51:56 +0200

    added default livecharts typeface

--- ssprea-nvidia-control/App.axaml.cs
+++ ssprea-nvidia-control/App.axaml.cs
@@ -6,6 +6,10 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using SkiaSharp;
 using ssprea_nvidia_control.ViewModels;
 using ssprea_nvidia_control.Views;
 using ssprea_nvidia_control.Models;
@@ -25,18 +29,23 @@ public partial class App : Application
         
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            
-            
-            // Line below is needed to remove Avalonia data validation.
-            // Without this line you will get duplicate validations from both Avalonia and CT
-            BindingPlugins.DataValidators.RemoveAt(0);
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainWindowViewModel(),
                 
             };
             
-            
+            LiveCharts.Configure(config =>
+                {
+                    
+                    config.HasTextSettings(new TextSettings()
+                    {
+                        DefaultTypeface = SKTypeface.FromStream(AssetLoader.Open(
+                            new Uri("avares://ssprea-nvidia-control/Assets/Fonts/NotoSans/NotoSans-Light.ttf")))
+                    });
+                }
+                // o SKFontManager.Default.MatchFamily("...")
+            );
         }
         
         
